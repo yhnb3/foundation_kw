0f8557f47bd93f967230baa0e05b9add6ce7f15d
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-07-23 21:36:50 +0900

    first commit

---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,11 @@
+import styles from './home.module.scss'
+
+const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.homeItem}>home</div>
+    </div>
+  )
+}
+
+export default Home
